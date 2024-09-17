c79a517e83124d2c5bfb14797ba3271cce28431c
Zigzypuff <romeo10651@gmail.com>
2024-09-17 16:38:45 +0200

    Added graphics and several features
    graphics added for the background and input button and borders for the rectangles, the window is now resizable, characters are capitalized as you type them so you don't have to hold shift now, auto-deletes invalid answers, now also shows a max streak for the session

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ function love.load()
     max = 3
     factor = math.floor(255/max)
     text = ""
+    upperText = ""
     response = 0
     colorAnswerShown = ""
     red = love.math.random(0, max)*factor
@@ -36,6 +37,11 @@ function love.load()
     char6 = 0
     valid = 0
     streak = 0
+    maxStreak = streak
+    background = love.graphics.newImage("Background.png")
+    colorBorder = love.graphics.newImage("SquareBorder.png")
+    font = love.graphics.newFont(30)
+    inputButton = love.graphics.newImage("inputbutton.png")
 end
 
 function createColor()
@@ -55,11 +61,22 @@ function createColor()
         colorAnswer = tostring(rhex1)..tostring(rhex2)..tostring(ghex1)..tostring(ghex2)..tostring(bhex1)..tostring(bhex2)
 end
 
+function windowChangeHeight()
+    local windowHeight = love.graphics.getHeight()
+    return windowHeight, windowWidth
+end
+
+function windowChangeWidth()
+    local windowWidth = love.graphics.getWidth()
+    return windowWidth
+end
+
 function colorGuessed()
+    fullCaps()
     if text_length == 6 then
-        responseRed = string.sub(text, 1,1)..string.sub(text, 2,2)
-        responseGreen = string.sub(text, 3,3)..string.sub(text, 4,4)
-        responseBlue = string.sub(text, 5,5)..string.sub(text, 6,6)
+        responseRed = string.sub(upperText, 1,1)..string.sub(upperText, 2,2)
+        responseGreen = string.sub(upperText, 3,3)..string.sub(upperText, 4,4)
+        responseBlue = string.sub(upperText, 5,5)..string.sub(upperText, 6,6)
     else
         responseRed = 0
         responseGreen = 0
@@ -67,18 +84,49 @@ function colorGuessed()
     end
 end
 
+function fontSize()
+        if love.graphics.getWidth()<800 then
+            local size = 30
+            return size
+        elseif love.graphics.getWidth()>1300 then
+            local size = 50
+            return size
+        else
+            local size = 40 
+            return size
+    end
+end
+
+function fontScale()
+    if love.graphics.getWidth()<800 then
+            local size = 80
+            return size
+        elseif love.graphics.getWidth()>1300 then
+            local size = 120
+            return size
+        else
+            local size = 100
+            return size
+    end
+end
+
 function checkInput()
-        char1 = hexTable[(string.sub(text, 1,1))]
-        char2 = hexTable[(string.sub(text, 2,2))]
-        char3 = hexTable[(string.sub(text, 3,3))]
-        char4 = hexTable[(string.sub(text, 4,4))]
-        char5 = hexTable[(string.sub(text, 5,5))]
-        char6 = hexTable[(string.sub(text, 6,6))]
+        fullCaps()
+        char1 = hexTable[(string.sub(upperText, 1,1))]
+        char2 = hexTable[(string.sub(upperText, 2,2))]
+        char3 = hexTable[(string.sub(upperText, 3,3))]
+        char4 = hexTable[(string.sub(upperText, 4,4))]
+        char5 = hexTable[(string.sub(upperText, 5,5))]
+        char6 = hexTable[(string.sub(upperText, 6,6))]
     if char1 == nil or char2 == nil or char3 == nil or char4 == nil or char5 == nil or char6 == nil then
         return(1)
     else
         return(0)
     end
+end
+
+function fullCaps()
+    upperText = string.upper(text)
 end
 
 function convertBits(rbit, gbit, bbit)
@@ -92,19 +140,26 @@ function love.textinput(t)
 end
 
 function responseToPlayer()
-    if response == 1 and text == colorAnswer and text_length == 6 then
+    fullCaps()
+    if response == 1 and upperText == colorAnswer and text_length == 6 then
         colorAnswerShown = "Correct"
         colorGuessed()
         convertBits(responseRed, responseGreen, responseBlue)
         createColor()
         streak = streak + 1
+        if maxStreak <= streak then
+            maxStreak = streak
+        else
+            maxStreak = maxStreak
+        end
         response = 0
-    elseif response ==1 and text ~= colorAnswer and text_length == 6 then
+    elseif response ==1 and upperText ~= colorAnswer and text_length == 6 then
         if valid == 1 then
             rBitValue = 0
             gBitValue = 0
             bBitValue = 0
-            colorAnswerShown = "Submit a valid 6 character hex code Example: FFFFFF"
+            colorAnswerShown = ""
+            text=""
 
         else
             colorAnswerShown = "Wrong: "..tostring(colorAnswer)
@@ -112,6 +167,7 @@ function responseToPlayer()
             colorGuessed()
             convertBits(responseRed, responseGreen, responseBlue)
             createColor()
+            maxStreak = maxStreak
             streak = 0
         end
     else
@@ -120,9 +176,32 @@ function responseToPlayer()
 
 end
 
+function rectangleSide()
+    if 2.5*windowChangeHeight() < windowChangeWidth() then
+        local sideLength = windowChangeHeight()/4
+        return sideLength
+
+    elseif windowChangeHeight() > 1.5*windowChangeWidth() then
+        local sideLength = windowChangeWidth()/4
+        return sideLength
+
+    elseif windowChangeHeight() < windowChangeWidth() then
+        local sideLength = windowChangeWidth()/4
+        return sideLength
+    else
+        local sideLength = windowChangeHeight()/4
+        return sideLength
+    end
+end
+
+function borderScale()
+   local borderS = rectangleSide()/600
+   return borderS
+end
+
 function love.update(dt)
     text_length = string.len(text)
-
+    fontSize()
     if text_length <= 6 then      
         love.keyboard.setTextInput(true)
     end
@@ -140,7 +219,12 @@ function love.update(dt)
     else
         valid = 0
     end
-
+    if windowChangeHeight() < 400 then
+        love.window.setMode(windowChangeWidth(), 400, {resizable = true})
+    end
+    if windowChangeWidth() < 400 then
+        love.window.setMode(400, windowChangeHeight(), {resizable = true})
+    end
 end
 
 function love.keypressed(key)
@@ -158,15 +242,22 @@ function love.keypressed(key)
 end
 
 function love.draw()
-
+    for i = 0, love.graphics.getWidth()/background:getWidth() do
+        for j = 0, love.graphics.getHeight()/background:getHeight() do
+            love.graphics.draw(background, i*background:getWidth(), j*background:getHeight())
+        end
+    end
+    love.graphics.setFont(love.graphics.newFont(fontSize()))
     love.graphics.setColor(love.math.colorFromBytes(red, green, blue))
-    love.graphics.rectangle('fill', 40, 40, 200, 200, 0, 0)
+    love.graphics.rectangle('fill', (windowChangeWidth()/2-1.25*rectangleSide()), (windowChangeHeight()/10), rectangleSide(), rectangleSide(), 0, 0)
     love.graphics.setColor(love.math.colorFromBytes(rBitValue, gBitValue, bBitValue))
-    love.graphics.rectangle('fill', 440, 40, 200, 200, 0, 0)
+    love.graphics.rectangle('fill', (windowChangeWidth()/2)+0.25*rectangleSide(), (windowChangeHeight()/10), rectangleSide(), rectangleSide(), 0, 0)
     love.graphics.setColor(1,1,1)
-    --love.graphics.print(tostring(colorAnswer), 40, 400) --shows answer in hexcode under the color
-    love.graphics.rectangle('line', 40, 500, 200, 50, 0, 0)
-    love.graphics.print("Input: " .. text, 45, 515)
-    love.graphics.print(colorAnswerShown, 45, 565)
-    love.graphics.print("Streak: "..tostring(streak))
+    love.graphics.draw(inputButton, (windowChangeWidth()/2)-200, windowChangeHeight()-120)
+    love.graphics.print("Input: " .. string.upper(text), (windowChangeWidth()/2)-190, windowChangeHeight()-105)
+    love.graphics.print(colorAnswerShown, (windowChangeWidth()/2)-190, windowChangeHeight()-185)
+    love.graphics.print("Streak: "..tostring(streak), (windowChangeWidth()/2)-fontScale(), 5)
+    love.graphics.draw(colorBorder, (windowChangeWidth()/2-1.25*rectangleSide()), (windowChangeHeight()/10), 0, borderScale())
+    love.graphics.draw(colorBorder, (windowChangeWidth()/2)+0.25*rectangleSide(), (windowChangeHeight()/10), 0, borderScale())
+    love.graphics.print("Max: "..tostring(maxStreak), 10, 5)
 end
